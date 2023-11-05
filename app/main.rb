35493582c1984c525c23300c3bf0c9983b85ab25
ongaeshi <ongaeshi0621@gmail.com>
2023-11-06 02:26:19 +0900

    feat: 01-hello-dragon
    https://book.dragonriders.community/01-hello-dragon.html

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,9 @@
+def greet friend
+  "Hello #{friend}"
+end
+
 def tick args
-  args.outputs.labels  << [640, 540, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 500, 'Docs located at ./docs/docs.html and 100+ samples located under ./samples', 5, 1]
-  args.outputs.labels  << [640, 460, 'Join the Discord server! https://discord.dragonruby.org', 5, 1]
-
-  args.outputs.sprites << { x: 576,
-                            y: 280,
-                            w: 128,
-                            h: 101,
-                            path: 'dragonruby.png',
-                            angle: args.state.tick_count }
-
-  args.outputs.labels  << { x: 640,
-                            y: 60,
-                            text: './mygame/app/main.rb',
-                            size_enum: 5,
-                            alignment_enum: 1 }
+  args.outputs.labels << [120, 120, greet("Dragon")]
+  args.outputs.labels << [120, 100, greet("Oboe")]
+  args.outputs.sprites << [120, 280, 100, 80, "sprites/misc/dragon-0.png"]
 end
